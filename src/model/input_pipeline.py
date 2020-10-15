82cee1c138f9a0af0cd033310780b1b82677ae48
Abhijit Adhikary <abhi.drmc@gmail.com>
2020-10-16 07:41:56 +1100

    evaluation image dimension updated to 224, code was breaking previously

--- src/model/input_pipeline.py
+++ src/model/input_pipeline.py
@@ -20,6 +20,7 @@ mask_transform = transforms.Compose([
 
 eval_transform = transforms.Compose([
   transforms.ToTensor(),
+  transforms.Resize((224, 224)),
   transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
   transforms.ToPILImage()])
 

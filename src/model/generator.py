f99a0e64a43836622c4962acec93212a67d53afb
Abhijit Adhikary <abhi.drmc@gmail.com>
2020-10-24 11:23:22 +1100

    tanh applied again in the discriminator output

--- src/model/generator.py
+++ src/model/generator.py
@@ -163,6 +163,8 @@ class CResUNet(nn.Module):
     self.out_final = nn.Conv2d(in_channels=self.num_dense_blocks * self.num_channels_input,
                                out_channels=self.num_channels_output, kernel_size=1, stride=1)
 
+    self.tanh = nn.Tanh()
+
   def forward(self, image_masked, noise_tensor=None):
 
     if self.noise_tensor is not None:
@@ -203,7 +205,7 @@ class CResUNet(nn.Module):
     else:
       raise NotImplementedError
 
-    return out_final
+    return self.tanh(out_final)
 
 #
 #
